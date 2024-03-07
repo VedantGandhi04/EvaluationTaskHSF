01eee3263a7d987076915df044525bc0841a5520
VedantGandhi04 <vedant.gandhi345@gmail.com>
2024-03-07 18:54:42 +0530

    changestomodel

--- train_network.jl
+++ train_network.jl
@@ -13,13 +13,14 @@ df_test = df[(split_index + 1):end, :]
 #= Seperating the features and labels : 
  - X_train/test consists of 3 coordinate parameters(x, y, z) 
  - Y_train/test consists of 1 label which takes values ('s', 'b') =#
+# OneHotEncoding the labels - to convert this data into a form that can be fed to a machine learning model
 X_train = Matrix(df_train[:, 1:3])
 X_train = X_train'
-y_train = hcat(df_train[:, 4])
+y_train = hcat(Flux.onehotbatch((df_train[:, 4]),  ["s", "b"])[1, :])'
  
 X_test = Matrix(df_test[:, 1:3])
 X_test = X_test'
-y_test = hcat(df_test[:, 4])
+y_test = hcat(Flux.onehotbatch((df_test[:, 4]),  ["s", "b"])[1, :])'
  
 #Visualizing the data 
 Plot_data = scatter(df_train[:, 3], df_train[:, 2], df_train[:, 1],
@@ -31,35 +32,34 @@ Plot_data = scatter(df_train[:, 3], df_train[:, 2], df_train[:, 1],
     markersize = 1
 )
 display(Plot_data)
- 
-# OneHotEncoding the labels - to convert this data into a form that can be fed to a machine learning model
-diff = ["s", "b"];
-flux_y_train_onehot = onehotbatch(y_train, diff)
-flux_y_test_onehot = onehotbatch(y_test, diff)
- 
+  
 #= Building a model
-Given that our model has three inputs (3 features in every data point), and two outputs (2 different labels),
+Given that our model has three inputs (3 features in every data point), and one outputs (1 or 0),
 we don't have to initialize the parameters ourselves this time, instead Flux does it for us. =#
-flux_model = Chain(Dense(3=>2), softmax)
+flux_model = Chain(
+    Dense(3, 1, σ)
+)
+parameters = Flux.params(flux_model)
+
  
 #= Loss function
 Return the cross entropy. This is mathematically equivalent to crossentropy(softmax(ŷ), y),
 but is more numerically stable than using functions crossentropy and softmax separately. =#
 function flux_loss(flux_model, x, y)
     ŷ = flux_model(x)
-    Flux.logitcrossentropy(ŷ, y)
+    Flux.binarycrossentropy(ŷ, y)
 end;
  
  
 #= Accuracy function
 The onecold function would convert our output, which remember, are probability values, to the actual class names i.e. "s" and "b". =#
-flux_accuracy(x, y) = mean(Flux.onecold(flux_model(x), diff) .== y);
+flux_accuracy(x, y) = mean(Int.(flux_model(x) .>=0.5) .== y);
  
 #= Training the model
 'dLdm' includes both the gradients with respect to the weights and biases, 
 and you use this information to update both sets of parameters during the training process.  =#
-function train_flux_model!(flux_model, X_train, flux_y_train_onehot)
-    dLdm, _, _ = gradient(flux_loss, flux_model, X_train, flux_y_train_onehot)
+function train_flux_model!(flux_model, X, y)
+    dLdm, _, _ = gradient(flux_loss, flux_model, X, y)
     @. flux_model[1].weight -= 0.1 * dLdm[:layers][1][:weight]
     @. flux_model[1].bias -= 0.1 * dLdm[:layers][1][:bias]
 end;
@@ -69,9 +69,9 @@ To keep track of loss and accuracy, for each epoch we store it in loss_history a
 loss_history = Float64[]
 accuracy_history = Float64[]
 for i = 1:100
-    train_flux_model!(flux_model, X_train, flux_y_train_onehot);
+    train_flux_model!(flux_model, X_train, y_train);
  
-    push!(loss_history, flux_loss(flux_model, X_train, flux_y_train_onehot))
+    push!(loss_history, flux_loss(flux_model, X_train, y_train))
     push!(accuracy_history, flux_accuracy(X_train, y_train))
  
     flux_accuracy(X_train, y_train) >= 0.98 && break
@@ -80,10 +80,11 @@ end
  
 # Visualizing the loss and accuracy of our model
 Plot_loss = plot(1:length(loss_history), loss_history, label="Loss", xlabel="Epochs", ylabel="Loss", legend=:topright)
-# display(Plot_loss)
+display(Plot_loss)
 Plot_accuracy = plot!(1:length(accuracy_history), accuracy_history, label="Accuracy", xlabel="Epochs", ylabel="Accuracy", legend=:topright)
-# display(Plot_accuracy)
+display(Plot_accuracy)
  
+
 # Testing our model on test set
 @show flux_accuracy(X_test, y_test);
-@show flux_loss(flux_model, X_test, flux_y_test_onehot)
+@show flux_loss(flux_model, X_test, y_test)
