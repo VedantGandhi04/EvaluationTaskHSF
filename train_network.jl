c124b6c37f2c5522c31cc2bb1e8c69b2d79ed671
VedantGandhi04 <vedant.gandhi345@gmail.com>
2024-03-07 19:23:43 +0530

    finalchanges

--- train_network.jl
+++ train_network.jl
@@ -1,8 +1,8 @@
 # Importing the required Julia packages
 using Flux, Statistics, MLDatasets, DataFrames, OneHotArrays, Plots, CSV
- 
+
 # Importing the dataset
-df = CSV.read("dataset/dataset.csv", DataFrame)
+df = CSV.read("/home/vedant/JULIA/flux/dataset.csv", DataFrame)
  
 # Splitting the dataset into train and test sets in the ratio 4:1
 split_index = Int(0.8 * size(df, 1))
@@ -34,8 +34,8 @@ Plot_data = scatter(df_train[:, 3], df_train[:, 2], df_train[:, 1],
 display(Plot_data)
   
 #= Building a model
-Given that our model has three inputs (3 features in every data point), and one outputs (1 or 0),
-we don't have to initialize the parameters ourselves this time, instead Flux does it for us. =#
+Given that our model has three inputs (3 features in every data point), and one output (1 or 0), we don't have to initialize the parameters, Flux does it for us. 
+We use the sigmoid function because its value ranges from 0 to 1. =#
 flux_model = Chain(
     Dense(3, 1, σ)
 )
@@ -43,8 +43,7 @@ parameters = Flux.params(flux_model)
 
  
 #= Loss function
-Return the cross entropy. This is mathematically equivalent to crossentropy(softmax(ŷ), y),
-but is more numerically stable than using functions crossentropy and softmax separately. =#
+We use binarycrossentropy as it return the binary cross-entropy loss directly. =#
 function flux_loss(flux_model, x, y)
     ŷ = flux_model(x)
     Flux.binarycrossentropy(ŷ, y)
@@ -52,7 +51,8 @@ end;
  
  
 #= Accuracy function
-The onecold function would convert our output, which remember, are probability values, to the actual class names i.e. "s" and "b". =#
+We compare the predictions of the model with the actual values and calculate its accuracy.
+Since the output is in decimal ranging from 0 to 1, we convert it to binary and then to Int64. =#
 flux_accuracy(x, y) = mean(Int.(flux_model(x) .>=0.5) .== y);
  
 #= Training the model
@@ -64,11 +64,11 @@ function train_flux_model!(flux_model, X, y)
     @. flux_model[1].bias -= 0.1 * dLdm[:layers][1][:bias]
 end;
  
-#= The loop can be adjusted as per the user's needs, and the conditions can be specified in plain Julia. Here we will train the model for a maximum of 100 epochs, but to ensure that the model does not overfit, we will break as soon as our accuracy value crosses or becomes equal to 0.98.
+#= The loop can be adjusted as per the user's needs, and the conditions can be specified in plain Julia. Here we will train the model for a maximum of 50 epochs, but to ensure that the model does not overfit, we will break as soon as our accuracy value crosses or becomes equal to 0.98.
 To keep track of loss and accuracy, for each epoch we store it in loss_history and accuracy_history.  =#
 loss_history = Float64[]
 accuracy_history = Float64[]
-for i = 1:100
+for i = 1:50
     train_flux_model!(flux_model, X_train, y_train);
  
     push!(loss_history, flux_loss(flux_model, X_train, y_train))
@@ -85,6 +85,6 @@ Plot_accuracy = plot!(1:length(accuracy_history), accuracy_history, label="Accur
 display(Plot_accuracy)
  
 
-# Testing our model on test set
+# Testing our model on test_set
 @show flux_accuracy(X_test, y_test);
 @show flux_loss(flux_model, X_test, y_test)
